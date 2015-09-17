5155c42223d45c76bdcf63f0f602decc673e612a
Alex Khovansky <alx.khovansky@gmail.com>
2015-09-17 23:01:40 +0300

    Remove test migration

--- lib/database.rb
+++ lib/database.rb
@@ -11,34 +11,36 @@ class Database
 
 end
 
-class Create < ActiveRecord::Migration
-
-  def initialize
-    dbconfig = YAML::load(File.open(File.join(File.dirname(__FILE__), 'config/database.yml')))
-    ActiveRecord::Base.establish_connection(dbconfig)
-  end
-
-  def up
-
-    create_table "adverts" do |t|
-      t.string   "declarant"
-      t.string   "advtype"
-      t.string   "permission"
-      t.string   "road"
-      t.integer  "house"
-      t.string   "letter"
-      t.integer  "housing"
-      t.string   "clarification"
-      t.string   "crossroad"
-      t.string   "crossroadhouse"
-      t.string   "lettercrossroadhouse"
-      t.string   "housingcrossroadhouse"
-      t.string   "clarcrossroadhouse"
-      t.datetime "created_at",            null: false
-      t.datetime "updated_at",            null: false
-      t.string   "photo"
-    end
-
-  end
-
-end
+# Dirty migration for testing purposes
+#
+# class Create < ActiveRecord::Migration
+#
+#   def initialize
+#     dbconfig = YAML::load(File.open(File.join(File.dirname(__FILE__), 'config/database.yml')))
+#     ActiveRecord::Base.establish_connection(dbconfig)
+#   end
+#
+#   def up
+#
+#     create_table "adverts" do |t|
+#       t.string   "declarant"
+#       t.string   "advtype"
+#       t.string   "permission"
+#       t.string   "road"
+#       t.integer  "house"
+#       t.string   "letter"
+#       t.integer  "housing"
+#       t.string   "clarification"
+#       t.string   "crossroad"
+#       t.string   "crossroadhouse"
+#       t.string   "lettercrossroadhouse"
+#       t.string   "housingcrossroadhouse"
+#       t.string   "clarcrossroadhouse"
+#       t.datetime "created_at",            null: false
+#       t.datetime "updated_at",            null: false
+#       t.string   "photo"
+#     end
+#
+#   end
+#
+# end
